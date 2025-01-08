f2f65ff9bd27bf5938d0f8b127f2b146a3c63ec9
Sudipta Shaw <sudipta.shaw@brand-scapes.com>
2025-01-08 12:47:08 +0530

    changed deployment.py file

--- backend/crud/wsgi.py
+++ backend/crud/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'backend.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'backend.settings'
+settings_module = 'crud.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'crud.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
  
