48ef6f7e23a93eda519d2cdd2a35105325a6988f
sudipt-brandscapes <sudipta-brand-scapes.com>
2025-02-24 16:08:26 +0530

    add asgi.py

--- backend/crud/asgi.py
+++ backend/crud/asgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
-import os
-
-from django.core.asgi import get_asgi_application
+import os 
+from django.core.asgi import get_asgi_application 
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_asgi_application()
+application = WhiteNoise(application)
--- backend/crud/wsgi.py
+++ backend/crud/wsgi.py
@@ -8,12 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application 
+from whitenoise import WhiteNoise
 
-from django.core.wsgi import get_wsgi_application
 
 settings_module = 'crud.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'crud.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
  
-
 application = get_wsgi_application()
+application = WhiteNoise(application)
